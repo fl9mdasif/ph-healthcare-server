d8e2bac0a8a318bba74f5e81b57aa8636c2e2032
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2024-04-29 15:49:15 +0600

    [done]- change pass, forget pass, reset-pass etc

--- src/app/modules/admin/router.admin.ts
+++ src/app/modules/admin/router.admin.ts
@@ -2,20 +2,36 @@ import express from "express";
 import { AdminController } from "./controller.admin";
 import validateRequest from "../../middlewares/validateRequest";
 import { adminValidationSchemas } from "./validation.admin";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 router.get("/", AdminController.getAllFromDB);
 
-router.get("/:id", AdminController.getByIdFromDB);
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getByIdFromDB
+);
+
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(adminValidationSchemas.update),
   AdminController.updateIntoDB
 );
 
-router.delete("/:id", AdminController.deleteFromDB);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteFromDB
+);
 
-router.delete("/soft/:id", AdminController.softDeleteFromDB);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.softDeleteFromDB
+);
 
 export const AdminRoutes = router;
--- src/app/modules/auth/routes.auth.ts
+++ src/app/modules/auth/routes.auth.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import { AuthController } from "./controller.auth";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
+// import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/login", AuthController.loginUser);
 
 router.post("/refresh-token", AuthController.refreshToken);
+
+router.post(
+  "/change-password",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  AuthController.changePassword
+);
+
+router.post("/forgot-password", AuthController.forgotPassword);
+
+router.post("/reset-password", AuthController.resetPassword);
 
 export const AuthRoutes = router;
